ce9d839634d7144f0f4d6bd3c74298eb30bebf65
Moumita <48108230+moumitahmad@users.noreply.github.com>
2022-06-23 12:18:00 +0200

    begin include navigator components

--- app/src/main/java/com/example/leftlovers/MainActivity.java
+++ app/src/main/java/com/example/leftlovers/MainActivity.java
@@ -1,26 +1,26 @@
 package com.example.leftlovers;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+/*import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentTransaction;*/
 
 import android.os.Bundle;
-import android.util.Log;
+// import android.util.Log;
 
-import com.example.leftlovers.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        showPage(new SearchFragment());
+        setContentView(R.layout.activity_main);
 
-        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+        /*binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        showPage(new SearchFragment());*/
+
+/*        binding.bottomNavigation.setOnItemSelectedListener(item -> {
             if(item.getItemId() == R.id.page_fridge) {
                 Log.i("Nav-Check", "Fridge page");
                 showPage(new FridgeFragment());
@@ -33,13 +33,13 @@ public class MainActivity extends AppCompatActivity {
             }
 
             return true;
-        });
+        });*/
     }
 
-    private void showPage(Fragment fragment) {
+    /*private void showPage(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout_main, fragment);
         fragmentTransaction.commit();
-    }
+    }*/
 }
