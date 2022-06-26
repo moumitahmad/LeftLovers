5070307c245ee641f70f90a6a4211e9a35137ae6
Moumita <48108230+moumitahmad@users.noreply.github.com>
2022-06-26 19:18:12 +0200

    remove old code

--- app/src/main/java/com/example/leftlovers/MainActivity.java
+++ app/src/main/java/com/example/leftlovers/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.leftlovers;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,31 +21,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = navHostFragment.getNavController();
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         NavigationUI.setupWithNavController(bottomNav, navController);
-
-        
-
-
-
-/*        binding.bottomNavigation.setOnItemSelectedListener(item -> {
-            if(item.getItemId() == R.id.page_fridge) {
-                Log.i("Nav-Check", "Fridge page");
-                showPage(new FridgeFragment());
-            } else if(item.getItemId() == R.id.page_bookmark) {
-                Log.i("Nav-Check", "Bookmark page");
-                showPage(new BookmarkFragment());
-            } else {
-                Log.i("Nav-Check", "Search page");
-                showPage(new SearchFragment());
-            }
-
-            return true;
-        });*/
     }
-
-    /*private void showPage(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout_main, fragment);
-        fragmentTransaction.commit();
-    }*/
 }
