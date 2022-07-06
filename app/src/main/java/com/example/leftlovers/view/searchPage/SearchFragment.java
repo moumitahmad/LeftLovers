aa760313cbf5f84afd85602c0a3b78a5912a1204
Moumita <48108230+moumitahmad@users.noreply.github.com>
2022-07-06 14:17:16 +0200

    fix merge conflicts, syling

--- app/src/main/java/com/example/leftlovers/view/searchPage/SearchFragment.java
+++ app/src/main/java/com/example/leftlovers/view/searchPage/SearchFragment.java
@@ -3,14 +3,10 @@ package com.example.leftlovers.view.searchPage;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.SearchView;
 
 import com.example.leftlovers.R;
 
@@ -19,9 +15,6 @@ import com.example.leftlovers.R;
  * create an instance of this fragment.
  */
 public class SearchFragment extends Fragment {
-
-    // private String IMAGE_EXAMPLE = "https://www.vegini.at/wp-content/uploads/2017/10/Rezeptbild-Spaghetti.jpg";
-    // private String RECIPE_LINK = "https://www.gutekueche.at/spaggetti-bolognese-rezept-22388";
 
     public SearchFragment() {
         // Required empty public constructor
@@ -37,22 +30,6 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-
-        // setup buttons
-        SearchView search = view.findViewById(R.id.searchView);
-        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                NavDirections action = (NavDirections) SearchFragmentDirections.actionSearchFragmentToSearchResultFragment(query);
-                Navigation.findNavController(view).navigate(action);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
 
         return view;
     }
