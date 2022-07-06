ca8193485c18c166da69db23c19ab254ed7d52c3
tseevers91 <49007348+tseevers91@users.noreply.github.com>
2022-07-06 21:51:29 +0200

    getList

--- app/src/main/java/com/example/leftlovers/view/searchPage/SearchFragment.java
+++ app/src/main/java/com/example/leftlovers/view/searchPage/SearchFragment.java
@@ -4,17 +4,27 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.leftlovers.R;
+import com.example.leftlovers.database.ApiConnection;
+import com.example.leftlovers.model.Recipe;
+import com.example.leftlovers.service.ReceipeDataService;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
 public class SearchFragment extends Fragment {
+
+    private ReceipeDataService receipeDataService;
+    private List recipeListTest;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -23,6 +33,8 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        receipeDataService = new ReceipeDataService(getActivity());
+
     }
 
     @Override
@@ -30,6 +42,20 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        receipeDataService.getRecipeList("egg", new ApiConnection.ListVolleyResponseListener() {
+                    @Override
+                    public void onError(String message) {
+                        Log.d("Api Connection Error", message);
+                    }
+
+                    @Override
+                    public void onResponse(List<Recipe> recipeList) {
+                        recipeListTest = recipeList;
+                        Log.d("List", recipeListTest.toString());
+                    }
+                }
+        );
 
         return view;
     }
