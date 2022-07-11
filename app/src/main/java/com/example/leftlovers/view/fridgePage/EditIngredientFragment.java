5310e8cc715b3bf634dd977e88836bbc0a37b04a
Moumita <48108230+moumitahmad@users.noreply.github.com>
2022-07-11 13:49:12 +0200

    image upload from gallery

--- app/src/main/java/com/example/leftlovers/view/fridgePage/EditIngredientFragment.java
+++ app/src/main/java/com/example/leftlovers/view/fridgePage/EditIngredientFragment.java
@@ -1,22 +1,41 @@
 package com.example.leftlovers.view.fridgePage;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.leftlovers.R;
 import com.example.leftlovers.model.Ingredient;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -24,11 +43,15 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EditIngredientFragment extends Fragment {
+
+    private ActivityResultLauncher resultLauncher;
+    private String uploadImagePath;
 
     private static final int DEFAULT_AMOUNT = 1;
     private Ingredient chosenIngredient;
@@ -49,8 +72,24 @@ public class EditIngredientFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            Log.d("IMG RESULT", uri.toString());
+                            ImageView ingredientImage = getView().findViewById(R.id.ingredient_image);
+                            ingredientImage.setImageURI(uri);
+                            // TODO: save image in local database
+                        }
+                    }
+                }
+        );
         chosenIngredient = EditIngredientFragmentArgs.fromBundle(getArguments()).getChosenIngredient();
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -102,6 +141,16 @@ public class EditIngredientFragment extends Fragment {
             }
         });
 
+        // upload button
+        FloatingActionButton uploadButton = view.findViewById(R.id.upload_button);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImg(getContext());
+            }
+        });
+
+        // save button
         Button saveButton = view.findViewById(R.id.save_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,6 +184,31 @@ public class EditIngredientFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void selectImg(Context context) {
+        // create dialog
+        final CharSequence[] options = {"Choose from your gallery", "Cancel"};
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(context);
+        dialogBuilder.setTitle(R.string.upload_title);
+        dialogBuilder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                switch(item) {
+                    case 0:  // "Choose from your gallery"
+                        Log.d("UPLOAD DIALOG", "gallery");
+                        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                        galleryIntent.setType("image/jpg");
+                        galleryIntent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                        resultLauncher.launch(galleryIntent);
+                        break;
+                    case 1: // Cancel
+                        Log.d("UPLOAD DIALOG", "gallery");
+                        dialog.dismiss();
+                }
+            }
+        });
+        dialogBuilder.show();
     }
 
     private List<InputError> validateInputs() {
